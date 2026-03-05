3ad37a54cf62b32a9515f4af9c3c11a85c0fe83a
LukReis <lucasesdras8@gmail.com>
2026-03-04 21:49:26 -0300

    Corrigindo .gitignore

--- src/main/java/com/rasmoo/api/querango/repository/CardapioRepository.java
+++ src/main/java/com/rasmoo/api/querango/repository/CardapioRepository.java
@@ -5,7 +5,6 @@ import com.rasmoo.api.querango.entity.Cardapio;
 import com.rasmoo.api.querango.repository.projection.CardapioProjection;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -13,7 +12,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+
 
 @Repository
 public interface CardapioRepository extends PagingAndSortingRepository<Cardapio, Integer>, JpaSpecificationExecutor<Cardapio> {
